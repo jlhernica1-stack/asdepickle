468a06d7b525a91c2ccd1135468ab6096b87246b
jlhernica1-stack <jlhernica1@gmail.com>
2026-04-14 21:26:09 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#"]').forEach(function(a) {
+  a.addEventListener('click', function(e) {
+    var target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
